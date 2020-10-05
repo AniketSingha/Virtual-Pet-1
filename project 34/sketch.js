30fc015f753ac545fcfeb3f3f32cad84775d1480
AniketSingha <69382836+AniketSingha@users.noreply.github.com>
2020-10-05 14:17:27 +0530

    Add files via upload

---
+++ project 34/sketch.js
@@ -0,0 +1,26 @@
+var Dog;
+var happyDog;
+var database;
+var foodS;
+var foodStock;
+
+function preload()
+{
+dog=loadImage("sprit)	
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
